3c64504d0b708dfa42d931319ebe45f3a0ca261b
plucky <plucky@echo.engineer>
2023-02-11 16:03:27 +0800

    commit by easydoc compiler at 1676102602378

--- statics/js/main.js
+++ statics/js/main.js
@@ -594,3 +594,26 @@ function urlParams(url) {
     });
     return obj;
 }
+
+// 显示一个Toast，提示消息
+var toast = (content, time) => {
+    return new Promise((resolve, reject) => {
+        let elAlertMsg = document.querySelector("#fehelper_alertmsg");
+        if (!elAlertMsg) {
+            let elWrapper = document.createElement('div');
+            elWrapper.innerHTML = '<div id="fehelper_alertmsg" style="position:fixed;top:50%;left:50%;right:0;z-index:100">' +
+                '<p style="background:#000;display:inline-block;color:#fff;text-align:center;' +
+                'padding:10px 10px;margin:0 auto;font-size:14px;border-radius:4px;">' + content + '</p></div>';
+            elAlertMsg = elWrapper.childNodes[0];
+            document.body.appendChild(elAlertMsg);
+        } else {
+            elAlertMsg.querySelector('p').innerHTML = content;
+            elAlertMsg.style.display = 'block';
+        }
+
+        window.setTimeout(function () {
+            elAlertMsg.style.display = 'none';
+            resolve && resolve();
+        }, time || 1000);
+    });
+};
